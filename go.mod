b9f17d749f0adf32d14820176da6aeb0fb9ca1b6
Furm <30990507+JoshuaFurman@users.noreply.github.com>
2023-04-12 12:12:29 -0600

    Added terminal streaming & updated to use gpt-4 endpoint

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/JoshuaFurman/ask-ai
 
-go 1.19
+go 1.20
 
-require github.com/sashabaranov/go-gpt3 v0.0.0-20220811094137-be08f204f03a // indirect
+require github.com/sashabaranov/go-openai v1.7.0
--- main.go
+++ main.go
@@ -2,32 +2,21 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 
-	gogpt "github.com/sashabaranov/go-gpt3"
+	openai "github.com/sashabaranov/go-openai"
 )
 
-func build_prompt(question string) string {
-	return fmt.Sprintf(`
-You are GPT-3 a highly advanced AI assistant that is an expert in all fields and you provide high quality answers.
-
-I need your help answering the following question and please provides examples when you can:
-
-%s
-`, question)
-}
-
 func main() {
-	// #########################################################################################
-	// SET UP
-	// #########################################################################################
+	// Configure the API client
 	bearer := os.Getenv("OPEN_AI_TOKEN")
 	if bearer == "" {
 		fmt.Println("You need to set OPEN_AI_TOKEN environment variable.")
 	}
-
 	preTemp := os.Getenv("OPEN_AI_TEMP") // Pull env var, convert and error check
 	if preTemp == "" {
 		fmt.Println("You need to set OPEN_AI_TEMP environment variable.")
@@ -36,7 +25,6 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	preMaxTokens := os.Getenv("OPEN_AI_MAX_TOKENS") // Pull env var, convert and error check
 	if preMaxTokens == "" {
 		fmt.Println("You need to set OPEN_AI_MAX_TOKENS evironment variable.")
@@ -46,34 +34,51 @@ func main() {
 		fmt.Println(err)
 	}
 
+	// Get prompt and validate
 	var prompt string
 	if len(os.Args) < 2 {
 		fmt.Println("No command line argument supplied!")
 		return
 	} else {
-		prompt = os.Args[1] // CLI argument provided as prompt to GPT-3
+		prompt = os.Args[1] // CLI argument provided as prompt to GPT-4
 	}
-	// #########################################################################################
-	// #########################################################################################
-	// #########################################################################################
 
-	c := gogpt.NewClient(bearer)
+	// Create GPT stream chat request
+	c := openai.NewClient(bearer)
 	ctx := context.Background()
-
-	// Make call to OpenAI API
-	req := gogpt.CompletionRequest{
-		Model:       "text-davinci-003",
+	req := openai.ChatCompletionRequest{
+		Model:       openai.GPT4,
 		MaxTokens:   maxTokens,
 		Temperature: float32(temperature),
-		Prompt:      build_prompt(prompt), // Using build_prompt function to inject prompt template
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: prompt,
+			},
+		},
+		Stream: true,
 	}
-	resp, err := c.CreateCompletion(ctx, req)
+	stream, err := c.CreateChatCompletionStream(ctx, req)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("ChatCompletionStream error: %v\n", err)
 		return
 	}
+	defer stream.Close()
 
-	// Print results
-	fmt.Println(resp.Choices[0].Text)
-	fmt.Printf("\nTotal Tokens used: %d\n", resp.Usage.TotalTokens)
+	fmt.Println()
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			// fmt.Println("\nStream finished")
+			fmt.Println()
+			return
+		}
+
+		if err != nil {
+			fmt.Printf("\nStream error: %v\n", err)
+			return
+		}
+
+		fmt.Printf(response.Choices[0].Delta.Content)
+	}
 }
