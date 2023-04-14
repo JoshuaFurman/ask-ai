8414ef897b840d524141b50b5ae8895c52ab09db
Furm <30990507+JoshuaFurman@users.noreply.github.com>
2023-04-14 16:50:47 -0600

    Added chat mode. Cleaned up some output and error handling.

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"errors"
 	"fmt"
 	"io"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/alecthomas/chroma"
 	"github.com/alecthomas/chroma/formatters"
@@ -123,62 +125,142 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// Get prompt and validate
-	system_prompt := "You are a super powerful AI assistant. Answer all queries as concisely as possible and try to think through each response step-by-step."
-	var prompt string
 	if len(os.Args) < 2 {
+		fmt.Println()
 		fmt.Println("No command line argument supplied!")
+		fmt.Println("Please use: --chat or -c for chat mode. Type \"exit\" to exit the chat.")
+		fmt.Println("Please use: \"Double Quotes\" or 'Single Quotes' for a one-off command.")
 		return
-	} else {
-		prompt = os.Args[1] // CLI argument provided as prompt to GPT-4
-	}
 
-	// Create GPT stream chat request
-	c := openai.NewClient(bearer)
-	ctx := context.Background()
-	req := openai.ChatCompletionRequest{
-		Model:       openai.GPT4,
-		MaxTokens:   maxTokens,
-		Temperature: float32(temperature),
-		Messages: []openai.ChatCompletionMessage{
-			{
-				Role:    openai.ChatMessageRoleSystem,
-				Content: system_prompt,
-			},
-			{
+	} else if os.Args[1] == "--chat" || os.Args[1] == "-c" {
+		client := openai.NewClient(bearer)
+		ctx := context.Background()
+		messages := make([]openai.ChatCompletionMessage, 0)
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println()
+		fmt.Println("Begin Conversation")
+		fmt.Println("------------------")
+		var fullMessage string
+
+		for {
+			fmt.Print("-> ")
+			text, _ := reader.ReadString('\n')
+			// convert CRLF to LF
+			text = strings.Replace(text, "\n", "", -1)
+			if text == "exit" {
+				return
+			}
+			messages = append(messages, openai.ChatCompletionMessage{
 				Role:    openai.ChatMessageRoleUser,
-				Content: prompt,
+				Content: text,
+			})
+
+			stream, err := client.CreateChatCompletionStream(
+				ctx,
+				openai.ChatCompletionRequest{
+					Model:       openai.GPT4,
+					MaxTokens:   maxTokens,
+					Temperature: float32(temperature),
+					Messages:    messages,
+					Stream:      true,
+				},
+			)
+
+			if err != nil {
+				fmt.Printf("ChatCompletion error: %v\n", err)
+				continue
+			}
+			defer stream.Close()
+
+			// Used for processing code blocks
+			state := 0
+			buffer := ""
+			code := ""
+			lang := ""
+
+			for {
+				response, err := stream.Recv()
+				if errors.Is(err, io.EOF) {
+					fmt.Println() // For spacing of the respsonse
+					fmt.Println()
+					break
+				}
+				if err != nil {
+					fmt.Printf("\nStream error: %v\n", err)
+					return
+				}
+
+				for _, char := range response.Choices[0].Delta.Content {
+					processChar(char, &state, &buffer, &code, &lang)
+				}
+				fullMessage = fullMessage + response.Choices[0].Delta.Content // Save full response to save back to chat context
+
+			}
+			// Put response back into chat context
+			messages = append(messages, openai.ChatCompletionMessage{
+				Role:    openai.ChatMessageRoleAssistant,
+				Content: fullMessage,
+			})
+		}
+
+	} else if os.Args[1][0] == '"' || os.Args[1][0] == '\'' { // Checks the first char of the cli argument
+		// Get prompt and validate
+		system_prompt := "You are a super powerful AI assistant. Answer all queries as concisely as possible and try to think through each response step-by-step."
+		prompt := os.Args[1]
+
+		// Create GPT stream chat request
+		c := openai.NewClient(bearer)
+		ctx := context.Background()
+		req := openai.ChatCompletionRequest{
+			Model:       openai.GPT4,
+			MaxTokens:   maxTokens,
+			Temperature: float32(temperature),
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: system_prompt,
+				},
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt,
+				},
 			},
-		},
-		Stream: true,
-	}
-	stream, err := c.CreateChatCompletionStream(ctx, req)
-	if err != nil {
-		fmt.Printf("ChatCompletionStream error: %v\n", err)
-		return
-	}
-	defer stream.Close()
-
-	state := 0
-	buffer := ""
-	code := ""
-	lang := ""
-
-	fmt.Println()
-	for {
-		response, err := stream.Recv()
-		if errors.Is(err, io.EOF) {
-			fmt.Println() // For spacing of the respsonse
-			return
+			Stream: true,
 		}
-
+		stream, err := c.CreateChatCompletionStream(ctx, req)
 		if err != nil {
-			fmt.Printf("\nStream error: %v\n", err)
+			fmt.Printf("ChatCompletionStream error: %v\n", err)
 			return
 		}
+		defer stream.Close()
 
-		for _, char := range response.Choices[0].Delta.Content {
-			processChar(char, &state, &buffer, &code, &lang)
+		state := 0
+		buffer := ""
+		code := ""
+		lang := ""
+
+		fmt.Println()
+		for {
+			response, err := stream.Recv()
+			if errors.Is(err, io.EOF) {
+				fmt.Println() // For spacing of the response
+				return        // Finished displaying stream to terminal
+			}
+			if err != nil {
+				fmt.Printf("\nStream error: %v\n", err)
+				return
+			}
+
+			for _, char := range response.Choices[0].Delta.Content {
+				processChar(char, &state, &buffer, &code, &lang)
+			}
 		}
+	} else {
+		fmt.Println()
+		fmt.Println("***ERROR***")
+		fmt.Println("Sorry that was not a correct argument.")
+		fmt.Println("Please use: --chat or -c for chat mode. Type \"exit\" to exit the chat.")
+		fmt.Println("Please use: \"Double Quotes\" or 'Single Quotes' for a one-off command.")
+		return
 	}
 }
